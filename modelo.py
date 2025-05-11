592e3b7bc4db59a0f3ad5aa75897b2357e8fd483
VintellX <vintellx@gmail.com>
2025-05-11 07:49:07 +0530

    Create train.ipynb

--- modelo.py
+++ modelo.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class VeriVoice(nn.Module):
     def __init__(self, mel_binos=80, embed_dim=128, num_heads=4, num_layers=2, num_classes=2, max_seq_len=1000):
         super().__init__()
-        self.graph_conv = nn.Conv1d(mel_binos, mel_binos, kernel_size=3, padding=1, groups=mel_bins)
+        self.graph_conv = nn.Conv1d(mel_binos, mel_binos, kernel_size=3, padding=1, groups=mel_binos)
         self.frame_embed = nn.Linear(mel_binos, embed_dim)
         self.pos_emb = nn.Parameter(torch.zeros(1, max_seq_len, embed_dim))
         encoder = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads)
